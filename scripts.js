f620a56b6b18ee6b875460474920e7ed0af8ea0a
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-02-09 07:47:23 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+// Get the current username from the cookies
+var user = cookie.get('user');
+if (!user) {
 
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('We cannot work with you like that!');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
+  }
+}
